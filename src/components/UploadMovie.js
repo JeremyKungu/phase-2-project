ad0db01363ae978ef7efef6454cc3c162fdc004e
JeremyKungu <93706398+JeremyKungu@users.noreply.github.com>
2022-10-07 12:50:45 +0300

    Update UploadMovie.js

--- src/components/UploadMovie.js
+++ src/components/UploadMovie.js
@@ -1,37 +1,28 @@
 import React from 'react'
 import {useState} from "react";
 
-function UploadMovie( {submit} ) {
-    const [movieData, setMovieData] = useState([]);
-    const [images, setImages] = useState([]);
+function UploadMovie({data}) { 
+  
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
 
-    function handleChange(e) {
-        e.preventDefault()
-        setMovieData({
-        ...movieData,
-        [e.target.name]: e.target.value,
-        })
+    function handleSubmit(obj) {
+      obj.preventDefault();
+      const newData = {title, year}
+      fetch("http://localhost:3000/movies", {
+      method: "POST",
+      headers:{"Content-type": "application/json"},
+      body:JSON.stringify(newData)
+      })    
     }
-
-    function submitData(e) {
-        e.preventDefault();
-        submit(movieData)
-    }
-
-    function onImageChange(e) {
-      setImages({
-        ...images,
-        [e.target.files]: e.target.value,
-      })
-    }
+  
   return (
-    <form className="movie-form">
+    <form className="movie-form" onSubmit={handleSubmit}>
         <div className="movie-elements">
-          <input type="text" name="name" placeholder="Name" onChange={handleChange}/>
-          <input type="text" name="description" placeholder="Description" onChange={handleChange}/>       
-          <input type="file" multiple accept="image/*" onChange={onImageChange} />
+          <input type="text" name="name" placeholder="Name" value={title} onChange={(e) => setTitle(e.target.value)}/>
+          <input type="text" name="description" placeholder="Description" value={year} onChange={(e) => setYear(e.target.value)}/>       
         </div>
-        <button className="submit" type='submit' onClick={submitData}>Add Movie</button>
+        <button className="submit" type='submit'>Add Movie</button>
     </form>
 
   )
